4677bec72029a14633f0a1b64132688335606121
Deadalive25 <alekseevdi1990@gmail.com>
2024-09-23 17:48:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            string Number1 = Console.ReadLine();
+            int intNumber1 = int.Parse(Number1);
+            Console.WriteLine("Введите второе число:");
+            string Number2 = Console.ReadLine();
+            int intNumber2 = int.Parse(Number2);
+            Console.WriteLine("Допустимые арифметические действия:");
+            Console.WriteLine("+ сложение");
+            Console.WriteLine("- вычитание");
+            Console.WriteLine("* умножение");
+            Console.WriteLine("/ деление");
+            Console.WriteLine();
+            Console.WriteLine("Выберите действие:");
+            string action = Console.ReadLine();
+            string strToPrint = $"{intNumber1} {action} {intNumber2} = ";
+            if (action == "+")
+                strToPrint += Convert.ToString(intNumber1 + intNumber2);
+            else if (action == "-")
+                strToPrint += Convert.ToString(intNumber1 - intNumber2);
+            else if (action == "/")
+                strToPrint += ((double)intNumber1 / intNumber2).ToString();
+            else if (action == "*")
+                strToPrint += (intNumber1 * intNumber2).ToString();
+            else Console.WriteLine("Некорректное действие!");
+            Console.WriteLine(strToPrint);
+        }
+    }
+}
